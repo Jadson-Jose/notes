1ac543e2cfdd4f37f5162565b73ec3026215c575
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-09 16:09:39 -0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,6 +19,8 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo "Login Submit";
+        echo $request->input('text_username');
+        echo '<br>';
+        echo $request->input('text_password');
     }
 }
