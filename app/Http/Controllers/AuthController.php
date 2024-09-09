6f5b1e0b2df2e9f26c5a44931544d6cd4fe01ce0
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-09 15:50:43 -0300

    sinc

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function logout()
+    {
+        echo "Logout";
+    }
+
+    public function loginSubmit(Request $request)
+    {
+        echo "Login Submit";
+    }
+}
