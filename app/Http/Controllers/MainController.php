0fe176d661adeee740962a8acafd30d2a75e8189
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-12 14:03:57 -0300

    sinc

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,16 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        // load  user's notes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo '<pre>';
+        print_r($user);
+        print_r($notes);
+
+        die();
+
         return view('home');
     }
 
-    public function newNote(){
+    public function newNote()
+    {
         echo "I'm creating new note.";
     }
 }
