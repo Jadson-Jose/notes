c880672802d7bfcda69ff16a6842273bc0fd6f63
Jadson Silva <jadsonjosesilva@gmail.com>
2024-09-18 16:25:00 -0300

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -5,7 +5,13 @@
                 <div class="col">
                     <h4 class="text-info">{{ $note['title'] }}</h4>
                     <small class="text-secondary"><span
-                            class="opacity-75 me-2">Created_at:</span><strong>{{ date('Y-m-d H:i:s') }}</strong></small>
+                            class="opacity-75 me-2">Created_at:</span><strong>{{ date('Y-m-d H:i:s', strtotime($note['created_at'])) }}</strong>
+                    </small>
+                    @if ($note['created_at'] != $note['updated_at'])
+                        <small class="text-secondary ms-5"><span
+                                class="opacity-75 me-2">Updated_at:</span><strong>{{ date('Y-m-d H:i:s', strtotime($note['updated_at'])) }}</strong>
+                        </small>
+                    @endif
                 </div>
                 <div class="col text-end">
                     <a href="{{ route('edit', ['id' => Crypt::encrypt($note['id'])]) }}"
